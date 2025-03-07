76d9db295990e990fc88d3e5624e534d05068e79
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2025-03-07 22:24:13 +0900

    [Silver I] Title: 무한부스터, Time: 248 ms, Memory: 21736 KB -BaekjoonHub

--- 백준/Silver/17391. 무한부스터/무한부스터.java
+++ 백준/Silver/17391. 무한부스터/무한부스터.java
@@ -16,16 +16,20 @@ public class Main {
 			   for(int k=0;k<2;k++) {
 				   int moveX = deltas[k][0];
 				   int moveY = deltas[k][1];
-	    			  
+//	    		   최대 움직일 수 있는 거리  
 				   int move = maps[i][j];
-	        		  
+
 				   while(move > 0) {  
+//					   X, Y가 움직일 수 있는 최대거리부터 0까지 줄여가며 계산
 					   int nowX = i+moveX*move; 
 					   int nowY = j+moveY*move;
+//					   보드판을 벗어나지 않는다면 
 					   if(nowX>=0 && nowX<N && nowY>=0 && nowY<M)
+//						   해당 X,Y 좌표에 처음 왔을 경우
 						   if(dp[nowX][nowY] == -1)
 							   dp[nowX][nowY] = dp[i][j]+1;
 						   else {
+//							   이미 방문한 적이 있어 값이 갱신되어 있는 경우
 							   dp[nowX][nowY] = Math.min(dp[i][j]+1, dp[nowX][nowY]);
 						   }
 					   	move--;
